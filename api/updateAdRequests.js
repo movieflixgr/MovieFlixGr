e5497d58731980467a0ac66e39926a319cc670eb
movieflixgr <121951544+movieflixgr@users.noreply.github.com>
2024-02-26 00:13:56 +0200

    Update updateAdRequests.js

--- api/updateAdRequests.js
+++ api/updateAdRequests.js
@@ -81,9 +81,10 @@ module.exports = async (req, res) => {
 // Find the index of the column containing the type in the header row
 let typeIndex = -1;
 if (values && values[0]) {
+  console.log("Value: " + values[0][2]);
   for (let i = 0; i < values[0].length; i++) {
-    if (values[0][i] === 'Banner') {
-      console.log(values[0][i]);
+    if (values[0][i] === "Banner") {
+      console.log("Value: " + values[0][i]);
       typeIndex = i;
       break;
     }
