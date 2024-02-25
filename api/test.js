c3cb5a0fb2de2b962dba1bea94e6197bff9f24d4
movieflixgr <121951544+movieflixgr@users.noreply.github.com>
2024-02-25 22:57:00 +0200

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,13 +1,20 @@
-module.exports = async (event, context) => {
+module.exports = async (req, res) => {
   try {
-    return {
-      statusCode: 200,
-      body: 'Hello Cron!'
-    };
+    // Perform any asynchronous operations here
+    // For example, fetching data from a database or an external API
+    // Simulating an asynchronous operation with setTimeout
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // Resolve the promise after 1000 milliseconds (1 second)
+        resolve();
+      }, 1000);
+    });
+
+    // Once the asynchronous operation is completed, send the response
+    res.status(200).end('Hello Cron!');
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal Server Error' })
-    };
+    // If an error occurs during the asynchronous operation, handle it here
+    console.error('An error occurred:', error);
+    res.status(500).end('Internal Server Error');
   }
 };
